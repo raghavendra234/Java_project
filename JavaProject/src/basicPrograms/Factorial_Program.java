f1b8dc15e229bc83cfafd46b78c468e9053f068a
Raghavendra.Damaraju <Raghavendra.Damaraju@IHOD31078.synechron.com>
2018-01-04 11:58:19 +0530

    factorial program

---
+++ JavaProject/src/basicPrograms/Factorial_Program.java
@@ -0,0 +1,21 @@
+package basicPrograms;
+
+import java.util.Scanner;
+
+public class Factorial_Program {
+
+	public static void main(String[] args) {
+	
+		int n,fact = 1;
+		System.out.println("Enter an integer to calculate it's factorial");
+		Scanner in = new Scanner(System.in);
+		n = in.nextInt();
+
+			for ( int c = 1 ; c <= n ; c++ )
+				fact = fact*c;
+			
+			
+			System.out.println("Factorial of "+n+" is = "+fact);
+			}
+					}
+
