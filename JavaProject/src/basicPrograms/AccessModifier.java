6d2dcd28a24f29cd5550612808e58edb7a53a650
Raghavendra.Damaraju <Raghavendra.Damaraju@IHOD31078.synechron.com>
2018-01-04 19:16:38 +0530

    Access Modifiers

---
+++ JavaProject/src/basicPrograms/AccessModifier.java
@@ -0,0 +1,47 @@
+package basicPrograms;
+
+class Access
+{
+
+	
+	public void A()
+	{
+		int a=10;
+		System.out.println(a);
+
+	}
+	private void AV()
+	{
+		int b=10;
+	}
+	protected void AVD()
+	{
+		int b=100;
+		System.out.println(b);
+	}
+	void ABCD()
+	{
+		int b=100;
+	}
+}
+
+
+
+public class AccessModifier  {
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+Access ac=new Access();
+ac.A();
+/*ac.AV();*/
+ac.AVD();
+
+
+
+	}
+
+}
