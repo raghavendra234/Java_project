3945252b07ee9123d9275a174e0e69f6b2eb85a9
Raghavendra.Damaraju <Raghavendra.Damaraju@IHOD31078.synechron.com>
2018-01-05 19:22:24 +0530

    Updated Program 2 revse of a string

---
+++ JavaProject/src/int_1/Program2.java
@@ -0,0 +1,25 @@
+
+/*
+ * 
+ * Write a program to revese of a string
+ * 
+ */
+
+package int_1;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+	String input = "GeeksForGeeks";
+	 
+    // convert String to character array
+    // by using toCharArray
+    char[] try1 = input.toCharArray();
+
+    for (int i = try1.length-1; i>=0; i--)
+        System.out.print(try1[i]);
+}
+
+	
+}
