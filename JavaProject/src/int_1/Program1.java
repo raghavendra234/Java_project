fea1712bfeb81777d6efdc453fbd98d8df1a691d
Raghavendra.Damaraju <Raghavendra.Damaraju@IHOD31078.synechron.com>
2018-01-05 16:35:02 +0530

    Updated with new programs

---
+++ JavaProject/src/int_1/Program1.java
@@ -0,0 +1,20 @@
+package int_1;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=2;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i+"");
+				
+				 sum=sum+i;
+			}
+			
+		}
+	System.out.println(sum);
+    	}
+
+}
